5bb83d6ef9735b74a103bfae8a3b0c422dd6fdc0
7ommyShelby <aryaalsaher@gmail.com>
2024-06-27 15:43:12 +0530

    manuall

--- index.js
+++ index.js
@@ -17,14 +17,27 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "null");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
+    const allowedOrigins = [
+      "https://threads-clone-frontend-mu.vercel.app",
+      "http://localhost:5173"
+    ];
+  
+    const origin = req.headers.origin;
+  
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
     }
+  
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+  
     next();
-});
+  });
 
 
 
